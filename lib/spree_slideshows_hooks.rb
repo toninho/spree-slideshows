f0565116db6badd18d4ed8c5e3c88e51e69e2e4e
Toninho Sousa <toninho.si@gmail.com>
2010-12-10 15:35:36 -0200

    using the hooks to include necessary javascripts and stylesheets files

--- lib/spree_slideshows_hooks.rb
+++ lib/spree_slideshows_hooks.rb
@@ -1,5 +1,26 @@
-class SpreeSlideshowsHooks < Spree::ThemeSupport::HookListener
+#require 'slideshows_helper'
+
+class SpreeSlideshowsHooks < Spree::ThemeSupport::HookListener  
   insert_after :admin_configurations_menu do
     %(<%= configurations_menu_item t('slideshow_slides'), admin_slides_path, t("manage_slideshow") %>)
   end
+  
+  insert_after :slideshow do
+    %(<%= insert_slideshow(:max => 2, :group => 'Home') %>)
+  end
+  
+  insert_after :inside_head do
+    %(<%= stylesheet_link_tag 'slideshow.css' %>
+      <%= javascript_include_tag 'jquery.cycle.lite.min.js' %>
+      <% javascript_tag do %>
+    	$(document).ready(function() {
+    	 $('.slideshow').cycle({
+    	fx: 'fade', // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+    	     timeout: 5000
+    	});
+    	});
+    <% end %>)
+  end
+  
+ 
 end
