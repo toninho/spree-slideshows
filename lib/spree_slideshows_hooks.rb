568a2506b969bbe683d9108a6827e16038232337
Toninho Sousa <toninho.si@gmail.com>
2010-12-10 11:42:11 -0200

    changing location of initilize and hook files

--- lib/spree_slideshows_hooks.rb
+++ lib/spree_slideshows_hooks.rb
@@ -1,7 +1,5 @@
-class SlideshowsHooks < Spree::ThemeSupport::HookListener
-
+class SpreeSlideshowsHooks < Spree::ThemeSupport::HookListener
   insert_after :admin_configurations_menu do
     %(<%= configurations_menu_item t('slideshow_slides'), admin_slides_path, t("manage_slideshow") %>)
   end
-
 end
